0f6a1698098b9f6e1b270467a1fb54dc497b9b8e
ananchai phahupongsub <ananchai@leaderplanet.co.th>
2018-10-30 10:25:02 +0700

    change icon color

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -18,6 +18,9 @@ const styles = {
    },
    imgLogo:{
     width: 190, height: 130, alt: 'Leader Planet',cursor: "pointer"
+   },
+   iconInNav:{
+    color: "black"
    }
 }
 class DefaultHeader extends Component {
@@ -52,14 +55,14 @@ class DefaultHeader extends Component {
         </Nav> */}
         <Nav className="ml-auto" navbar >
           <NavItem className="d-md-down-none">
-            <NavLink href="#"><i className="icon-bell"></i><Badge pill color="danger">5</Badge></NavLink>
+            <NavLink href="#"><i className="icon-bell" style={styles.iconInNav}></i><Badge pill color="danger">5</Badge></NavLink>
           </NavItem>
-          <NavItem className="d-md-down-none">
+          {/* <NavItem className="d-md-down-none">
             <NavLink href="#"><i className="icon-list"></i></NavLink>
           </NavItem>
           <NavItem className="d-md-down-none">
             <NavLink href="#"><i className="icon-location-pin"></i></NavLink>
-          </NavItem>
+          </NavItem> */}
           <AppHeaderDropdown direction="down">
             <DropdownToggle nav>
               <img src={'assets/img/avatars/6.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
@@ -81,7 +84,7 @@ class DefaultHeader extends Component {
             </DropdownMenu>
           </AppHeaderDropdown>
         </Nav>
-        <AppAsideToggler className="d-md-down-none" />
+        {/* <AppAsideToggler className="d-md-down-none" /> */}
         {/*<AppAsideToggler className="d-lg-none" mobile />*/}
       </React.Fragment>
     );
