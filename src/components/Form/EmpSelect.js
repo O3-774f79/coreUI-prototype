54a82aec02130a68f30aa7a3bdd4a289543ca65a
ananchai phahupongsub <ananchai@leaderplanet.co.th>
2018-11-15 14:52:24 +0700

    add component selectd

---
+++ src/components/Form/EmpSelect.js
@@ -0,0 +1,43 @@
+import React, { Component, Fragment } from 'react';
+
+import Select from 'react-select';
+export default class EmpSelect extends Component {
+  state = {
+    isClearable: true,
+    isDisabled: false,
+    isLoading: false,
+    isRtl: false,
+    isSearchable: true,
+    selectedOption: null
+  };
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption });
+    const {selectValue}= this.props
+    selectValue(selectedOption);
+  }
+  render() {
+    const {
+      isClearable,
+      isSearchable,
+      isDisabled,
+      selectedOption 
+    } = this.state;
+    return (
+      <Fragment>
+        <Select
+          className="basic-single"
+          classNamePrefix="select"
+          defaultValue={this.props.defaultBasicSelectValue}
+          isDisabled={isDisabled}
+          isLoading={this.props.loading}
+          value={selectedOption}
+          isClearable={isClearable}
+          isSearchable={isSearchable}
+          name="color"
+          options={this.props.data}
+          onChange={this.handleChange}
+        />
+      </Fragment>
+    );
+  }
+}
